a4a6d31d594eeeea404d47a3b2dbf6da3f761335
sourabhznwr <31401578+sourabhznwr@users.noreply.github.com>
2020-02-20 20:40:44 +0100

    lib_score

--- solution.py
+++ solution.py
@@ -1,19 +1,18 @@
 import numpy as np
 
-with open('data/a_example.txt') as f:
+
+
+with open('data/a.txt') as f:
     data = f.readlines()
-
-
 for i in range(len(data)):
     data[i] = data[i].split()
     for j in range(len(data[i])):
-        data[i] = [int(x) for x in data[i]]
-        
+        data[i] = [int(x) for x in data[i]]        
 n_books = data[0][0]
 n_lib = data[0][1]
 n_days = data[0][2]
 del data[0]
-book_scores = np.array(data[0])
+book_value = np.array(data[0])
 del data[0]
 
 n_books_lib = []
@@ -30,8 +29,16 @@ for i in range(n_lib):
 del i,j
 if not data:
     del data
-
-
 n_books_lib = np.array(n_books_lib)
 n_signup_days = np.array(n_signup_days)
 n_books_ship = np.array(n_books_ship)
+
+
+book_scores = []
+for i in range(len(books_in_lib)):
+    book_scores.append(0)
+    for j in range(len(books_in_lib[i])):
+        book_scores[i] = book_scores[i] + book_value[books_in_lib[i][j]]
+book_scores = np.array(book_scores)
+lib_scores = book_scores/((n_books_ship/n_books_lib)+n_signup_days)
+
